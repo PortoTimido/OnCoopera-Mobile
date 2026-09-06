a9047da5fc9f49f27e65c3c7de3dd16f30a0ceb0
carvalh0Henrique <carvalhoghenrique@gmail.com>
2026-09-06 14:06:23 -0300

    chore: atualiza configuracoes do Expo

--- jest.config.js
+++ jest.config.js
@@ -3,6 +3,6 @@ module.exports = {
   setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
   testMatch: ["<rootDir>/src/**/*.test.ts", "<rootDir>/src/**/*.test.tsx"],
   transformIgnorePatterns: [
-    "node_modules/(?!((jest-)?react-native|@react-native|expo(nent)?|@expo(nent)?/.*|expo-.*|@expo/.*|@expo-google-fonts/.*|@react-navigation/.*|nativewind|react-native-css|lucide-react-native|react-native-svg)/)",
+    "node_modules/(?!((jest-)?react-native|@react-native|expo(nent)?|@expo(nent)?/.*|expo-.*|@expo/.*|@expo-google-fonts/.*|@react-navigation/.*|nativewind|react-native-css|lucide-react-native|react-native-svg|standard-navigation)/)",
   ],
 };
